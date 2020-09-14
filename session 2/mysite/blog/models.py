23fea6601a0966f5e8cecb2005fe8b1d4ab7fffa
Tuan PHAM <phamtuantkt@gmail.com>
2020-09-14 17:10:33 +0700

    Session 2
    Add Session 2

---
+++ session 2/mysite/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+class BaiViet(models.Model):
+    tieude = models.CharField(max_length=300)
+    noidung = models.TextField()
+    ngay_dang = models.DateTimeField(default=timezone.now)
+    anhbia = models.ImageField(upload_to ="blog")
+
+class BinhLuan(models.Model):
+    post = models.ForeignKey('BaiViet', on_delete=models.CASCADE)
+    tacgia = models.CharField(max_length=200)
+    noidung = models.CharField(max_length=3000)
+    ngay_dang = models.DateTimeField(default=timezone.now)
+    chapnhan = models.BooleanField(default=False)
+
